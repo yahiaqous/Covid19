24a01eccee0d3082d1868edaa820059a0b0124d5
YAHIAQOUS <yahiaqous@gmail.com>
2023-01-29 20:32:38 +0300

    Created the Axios Instance and Fetching the Summary Data from the API

--- src/main_pages/Home.js
+++ src/main_pages/Home.js
@@ -1,9 +1,21 @@
 /** @format */
 
+import { useState, useEffect } from 'react';
+import AxiosInstance from '../components/axios/AxiosInstance';
 import Banner from '../components/Banner';
 import StatisticsRow from '../components/cards/StatisticsRow';
 
 export default function Home() {
+  const [globalData, setGlobalData] = useState([]);
+  const [countriesData, setCountriesData] = useState([]);
+
+  useEffect(() => {
+    AxiosInstance.get('/summary').then((res) => {
+      setGlobalData(res.data.Global);
+      setCountriesData(res.data.Countries);
+    });
+  }, []);
+
   return (
     <>
       {/* Banner */}
@@ -11,7 +23,7 @@ export default function Home() {
 
       {/* Global Statistics */}
       <div style={{ marginTop: '60px' }}>
-        <StatisticsRow title='GLOBAL STATISTICS' />
+        <StatisticsRow title='GLOBAL STATISTICS' data={globalData} />
       </div>
     </>
   );

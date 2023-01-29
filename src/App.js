e9653131b20544e43872eec29f1d04961d435735
YAHIAQOUS <yahiaqous@gmail.com>
2023-01-29 12:30:01 +0300

    Created the Initial Header, Home, and Footer Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 /** @format */
 
+import Header from './components/Header';
+import Home from './main_pages/Home';
+import Footer from './components/Footer';
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      {/* Header */}
+      <Header />
+
+      {/* Home Page */}
+      <Home />
+
+      {/* Footer */}
+      <Footer />
+    </>
+  );
 }
 
 export default App;

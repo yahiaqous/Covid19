142a569083e606ed83f7c96bc84d86b64c5ecdd0
YAHIAQOUS <yahiaqous@gmail.com>
2023-01-29 18:29:12 +0300

    Created the Global Statistics Cards with Junk Data

--- src/main_pages/Home.js
+++ src/main_pages/Home.js
@@ -1,11 +1,18 @@
 /** @format */
 
 import Banner from '../components/Banner';
+import StatisticsRow from '../components/cards/StatisticsRow';
 
 export default function Home() {
   return (
     <>
+      {/* Banner */}
       <Banner />
+
+      {/* Global Statistics */}
+      <div style={{ marginTop: '60px' }}>
+        <StatisticsRow title='GLOBAL STATISTICS' />
+      </div>
     </>
   );
 }

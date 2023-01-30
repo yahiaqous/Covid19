debc6bb2910da76d07516f0f27d4ea6cde73743c
YAHIAQOUS <yahiaqous@gmail.com>
2023-01-30 15:42:44 +0300

    Created the Countries Filter & Added the Mask GIF

--- src/components/cards/StatisticsRow.js
+++ src/components/cards/StatisticsRow.js
@@ -3,6 +3,7 @@
 
 import { Row, Col } from 'react-bootstrap';
 
+import CountriesFilter from '../search&filters/CountriesFilter';
 import StatisticCard from './StatisticCard';
 
 export default function StatisticsRow(props) {
@@ -25,11 +26,33 @@ export default function StatisticsRow(props) {
   ];
 
   return (
-    <Row style={{ margin: '0 40px 0 40px' }}>
-      <h1 style={{ fontWeight: 'bold' }}>{props.title?.toUpperCase()}</h1>
+    <Row style={{ margin: '0 40px 0 40px', justifyContent: 'center' }}>
+      {/* Row Header */}
+      <Row style={{ margin: '0', padding: '0' }}>
+        <Col lg={2} sm={3} style={{ padding: '0' }}></Col>
 
+        {/* Row Title */}
+        <Col lg={8} sm={6} style={{ padding: '0' }}>
+          <h1 style={{ fontWeight: 'bold' }}>{props.title?.toUpperCase()}</h1>
+        </Col>
+
+        {/* Countries Filter */}
+        <Col lg={2} sm={3} style={{ padding: '0 20px 0 20px' }}>
+          {props.allCountriesData && (
+            <div style={{ width: '100%', float: 'right' }}>
+              <CountriesFilter
+                allCountriesData={props.allCountriesData}
+                selectedCountryData={props.data}
+                setSelectedCountryData={props.setSelectedCountryData}
+              />
+            </div>
+          )}
+        </Col>
+      </Row>
+
+      {/* Cards */}
       {cardsData.map((item, key) => (
-        <Col key={key}>
+        <Col xl={4} md={6} key={key} style={{ padding: '0' }}>
           <StatisticCard
             title={item.title}
             value1={item.value1}
